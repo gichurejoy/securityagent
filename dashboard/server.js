64640f1a4708713a0a1f979a0f58ccf4c78e0615
joy.gichure <joy.gichure@medserv-africa.com>
2026-04-13 08:26:54 +0300

    Added dedicated server.js for cPanel hosting

---
+++ dashboard/server.js
@@ -0,0 +1,26 @@
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const dev = false
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+// Passenger provides the port via an environment variable
+const port = process.env.PORT || 3000
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true)
+      await handle(req, res, parsedUrl)
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err)
+      res.statusCode = 500
+      res.end('internal server error')
+    }
+  }).listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
